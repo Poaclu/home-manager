3099b7c7971762d142658a81b27b2a5b348b864a
Poaclu <25772718+Poaclu@users.noreply.github.com>
2024-12-08 15:48:47 +0100

    cleaning home.nix file

--- home.nix
+++ home.nix
@@ -1,8 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: let
   username = "poaclu";
-{
+in { 
+  imports = [
+    ./packages
+  ];
   home = {
-    username = ${username};
+    username = "${username}";
     homeDirectory = "/home/${username}";
     stateVersion = "24.05"; # Please read the comment before changing.
     packages = with pkgs; [
@@ -31,31 +34,29 @@
     # '';
     };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/poaclu/etc/profile.d/hm-session-vars.sh
-  #
-    sessionVariables = {
-      EDITOR = "nvim";
-    };
+    # Home Manager can also manage your environment variables through
+    # 'home.sessionVariables'. These will be explicitly sourced when using a
+    # shell provided by Home Manager. If you don't want to manage your shell
+    # through Home Manager then you have to manually source 'hm-session-vars.sh'
+    # located at either
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/poaclu/etc/profile.d/hm-session-vars.sh
+    #
+      sessionVariables = {
+        EDITOR = "nvim";
+      };
+  };
+
+  nixpkgs.config.allowUnfree = true;
 
   # Let Home Manager install and manage itself.
-  programs = {
-    home-manager.enable = true;
-    git = {
-      enable = true;
-      username = "Poaclu";
-      userEmail = "25772718+Poaclu@users.noreply.github.com"
+  programs.home-manager.enable = true;
 }
