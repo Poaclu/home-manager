e5c5faf794a503cbbb23845959beb053e94aa485
Poaclu <25772718+Poaclu@users.noreply.github.com>
2024-12-22 15:29:38 +0100

    importing hyprland config to nix language

--- home.nix
+++ home.nix
@@ -1,200 +1,203 @@
 { config, pkgs, ... }: let
-  username = "poaclu";
+username = "poaclu";
 in { 
-  home = {
-    username = "${username}";
-    homeDirectory = "/home/${username}";
-    stateVersion = "24.11"; # Please read the comment before changing.
-    packages = with pkgs; [
-      bat
-      brave
-      btop
-      discord
-      eza
-      fastfetch
-      fzf
-      git
-      obsidian
-      parsec-bin
-      starship
-      xfce.thunar
-      tldr
-      topgrade
-      zoxide
-    ];
-    file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+	imports = [
+	  ./packages/hypr/hyprland.nix
+	];
+	home = {
+		username = "${username}";
+		homeDirectory = "/home/${username}";
+		stateVersion = "24.11"; # Please read the comment before changing.
+			packages = with pkgs; [
+			bat
+				brave
+				btop
+				discord
+				eza
+				fastfetch
+				fzf
+				git
+				obsidian
+				parsec-bin
+				starship
+				xfce.thunar
+				tldr
+				topgrade
+				zoxide
+			];
+		file = {
+# # Building this configuration will create a copy of 'dotfiles/screenrc' in
+# # the Nix store. Activating the configuration will then make '~/.screenrc' a
+# # symlink to the Nix store copy.
+# ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-    };
+# # You can also set the file content immediately.
+# ".gradle/gradle.properties".text = ''
+#   org.gradle.console=verbose
+#   org.gradle.daemon.idletimeout=3600000
+# '';
+		};
 
-    # Home Manager can also manage your environment variables through
-    # 'home.sessionVariables'. These will be explicitly sourced when using a
-    # shell provided by Home Manager. If you don't want to manage your shell
-    # through Home Manager then you have to manually source 'hm-session-vars.sh'
-    # located at either
-    #
-    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-    #
-    # or
-    #
-    #  /etc/profiles/per-user/poaclu/etc/profile.d/hm-session-vars.sh
-    #
-    sessionVariables = {
-      EDITOR = "nvim";
-    };
-  };
+# Home Manager can also manage your environment variables through
+# 'home.sessionVariables'. These will be explicitly sourced when using a
+# shell provided by Home Manager. If you don't want to manage your shell
+# through Home Manager then you have to manually source 'hm-session-vars.sh'
+# located at either
+#
+#  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+#
+# or
+#
+#  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+#
+# or
+#
+#  /etc/profiles/per-user/poaclu/etc/profile.d/hm-session-vars.sh
+#
+		sessionVariables = {
+			EDITOR = "nvim";
+		};
+	};
 
-  nixpkgs.config.allowUnfree = true;
+	nixpkgs.config.allowUnfree = true;
 
-  # Let Home Manager install and manage itself.
-  programs = {
-    btop.enable = true;
-    fastfetch.enable = true;
-    git = {
-      enable = true;
-      userName = "Poaclu";
-      userEmail = "25772718+Poaclu@users.noreply.github.com";
-    };
-    home-manager.enable = true;
-    kitty = {
-      enable = true;
-      themeFile = "GruvboxMaterialDarkHard";
-      settings = {
-        font_family = "Liberation Mono";
-        bold_font = "auto";
-        italic_font = "auto";
-        bold_italic_font = "auto";
-        font_size = 8.0;
-        background_opacity = 0.7;
-        dynamic_background_opacity = "yes";
-      };
-    };
-    starship = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-    swaylock = {
-      enable = true;
-      settings = {
-        color = "1e1e2e";
-        bs-hl-color = "f5e0dc";
-        caps-lock-bs-hl-color = "f5e0dc";
-        caps-lock-key-hl-color = "a6e3a1";
-        inside-color = "00000000";
-        inside-clear-color = "00000000";
-        inside-caps-lock-color = "00000000";
-        inside-ver-color = "00000000";
-        inside-wrong-color = "00000000";
-        key-hl-color = "a6e3a1";
-        layout-bg-color = "00000000";
-        layout-border-color = "00000000";
-        layout-text-color = "cdd6f4";
-        line-color = "00000000";
-        line-clear-color = "00000000";
-        line-caps-lock-color = "00000000";
-        line-ver-color = "00000000";
-        line-wrong-color = "00000000";
-        ring-color = "b4befe";
-        ring-clear-color = "f5e0dc";
-        ring-caps-lock-color = "fab387";
-        ring-ver-color = "89b4fa";
-        ring-wrong-color = "eba0ac";
-        separator-color = "00000000";
-        text-color = "cdd6f4";
-        text-clear-color = "f5e0dc";
-        text-caps-lock-color = "fab387";
-        text-ver-color = "89b4fa";
-        text-wrong-color = "eba0ac";
-      };
-    };
-    topgrade.enable = true;
-    neovim.enable = true;
-    wlogout = {
-      enable = true;
-      layout = [
-        {
-          label = "lock";
-          action = "swaylock";
-          text = "Lock";
-          keybind = "l";
-        }
-        {
-          label = "hibernate";
-          action = "systemctl hibernate && swaylock";
-          text = "Hibernate";
-          keybind = "h";
-        }
-        {
-          label = "logout";
-          action = "loginctl terminate-user $USER";
-          text = "Logout";
-          keybind = "e";
-        }
-        {
-          label = "shutdown";
-          action = "systemctl poweroff";
-          text = "Shutdown";
-          keybind = "s";
-        }
-        {
-          label = "suspend";
-          action = "systemctl suspend && swaylock";
-          text = "Suspend";
-          keybind = "u";
-        }
-        {
-          label = "reboot";
-          action = "systemctl reboot";
-          text = "Reboot";
-          keybind = "r";
-        }
-      ];
-    };
-    zoxide = {
-      enable = true;
-      enableZshIntegration = true;
-    };
-    waybar = {
-      enable = true;
-    };
-    zsh = {
-      enable = true;
-      oh-my-zsh = {
-        enable = true;
-        plugins = [
-          "aliases"
-          "colored-man-pages"
-          "fzf"
-          "ufw"
-          "zsh-interactive-cd"
-        ];
-      };
-      initExtra = "source $HOME/.aliasrc\nsource $HOME/.shellrc";
-      shellAliases = {
-        cd = "z";
-        nix-rebuild = "sudo nixos-rebuild switch --flake /etc/nixos/";
-        home-rebuild = "home-manager switch --flake ~/.config/home-manager/";
-      };
-    };
-  };
-  xdg.configFile = {
-    "btop/btop.conf".source = ./packages/btop/btop.conf;
-    "../.aliasrc".source = ./packages/shell/aliasrc;
-    "../.bashrc".source = ./packages/shell/bashrc;
-    "../.shellrc".source = ./packages/shell/shellrc;
-    "topgrade.toml".source = ./packages/topgrade/topgrade.toml;
-    "hypr/hyprland.conf".source = ./packages/hypr/hyprland.conf;
-  };
+# Let Home Manager install and manage itself.
+	programs = {
+		btop.enable = true;
+		fastfetch.enable = true;
+		git = {
+			enable = true;
+			userName = "Poaclu";
+			userEmail = "25772718+Poaclu@users.noreply.github.com";
+		};
+		home-manager.enable = true;
+		kitty = {
+			enable = true;
+			themeFile = "GruvboxMaterialDarkHard";
+			settings = {
+				font_family = "Liberation Mono";
+				bold_font = "auto";
+				italic_font = "auto";
+				bold_italic_font = "auto";
+				font_size = 8.0;
+				background_opacity = 0.7;
+				dynamic_background_opacity = "yes";
+			};
+		};
+		starship = {
+			enable = true;
+			enableZshIntegration = true;
+		};
+		swaylock = {
+			enable = true;
+			settings = {
+				color = "1e1e2e";
+				bs-hl-color = "f5e0dc";
+				caps-lock-bs-hl-color = "f5e0dc";
+				caps-lock-key-hl-color = "a6e3a1";
+				inside-color = "00000000";
+				inside-clear-color = "00000000";
+				inside-caps-lock-color = "00000000";
+				inside-ver-color = "00000000";
+				inside-wrong-color = "00000000";
+				key-hl-color = "a6e3a1";
+				layout-bg-color = "00000000";
+				layout-border-color = "00000000";
+				layout-text-color = "cdd6f4";
+				line-color = "00000000";
+				line-clear-color = "00000000";
+				line-caps-lock-color = "00000000";
+				line-ver-color = "00000000";
+				line-wrong-color = "00000000";
+				ring-color = "b4befe";
+				ring-clear-color = "f5e0dc";
+				ring-caps-lock-color = "fab387";
+				ring-ver-color = "89b4fa";
+				ring-wrong-color = "eba0ac";
+				separator-color = "00000000";
+				text-color = "cdd6f4";
+				text-clear-color = "f5e0dc";
+				text-caps-lock-color = "fab387";
+				text-ver-color = "89b4fa";
+				text-wrong-color = "eba0ac";
+			};
+		};
+		topgrade.enable = true;
+		neovim.enable = true;
+		wlogout = {
+			enable = true;
+			layout = [
+			{
+				label = "lock";
+				action = "swaylock";
+				text = "Lock";
+				keybind = "l";
+			}
+			{
+				label = "hibernate";
+				action = "systemctl hibernate && swaylock";
+				text = "Hibernate";
+				keybind = "h";
+			}
+			{
+				label = "logout";
+				action = "loginctl terminate-user $USER";
+				text = "Logout";
+				keybind = "e";
+			}
+			{
+				label = "shutdown";
+				action = "systemctl poweroff";
+				text = "Shutdown";
+				keybind = "s";
+			}
+			{
+				label = "suspend";
+				action = "systemctl suspend && swaylock";
+				text = "Suspend";
+				keybind = "u";
+			}
+			{
+				label = "reboot";
+				action = "systemctl reboot";
+				text = "Reboot";
+				keybind = "r";
+			}
+			];
+		};
+		zoxide = {
+			enable = true;
+			enableZshIntegration = true;
+		};
+		waybar = {
+			enable = true;
+		};
+		zsh = {
+			enable = true;
+			oh-my-zsh = {
+				enable = true;
+				plugins = [
+					"aliases"
+						"colored-man-pages"
+						"fzf"
+						"ufw"
+						"zsh-interactive-cd"
+				];
+			};
+			initExtra = "source $HOME/.aliasrc\nsource $HOME/.shellrc";
+			shellAliases = {
+				cd = "z";
+				nix-rebuild = "sudo nixos-rebuild switch --flake /etc/nixos/";
+				home-rebuild = "home-manager switch --flake ~/.config/home-manager/";
+			};
+		};
+	};
+	xdg.configFile = {
+		"btop/btop.conf".source = ./packages/btop/btop.conf;
+		"../.aliasrc".source = ./packages/shell/aliasrc;
+		"../.bashrc".source = ./packages/shell/bashrc;
+		"../.shellrc".source = ./packages/shell/shellrc;
+		"topgrade.toml".source = ./packages/topgrade/topgrade.toml;
+#"hypr/hyprland.conf".source = ./packages/hypr/hyprland.conf;
+	};
 }
---
+++ packages/hypr/hyprland.nix
@@ -0,0 +1,196 @@
+{
+	wayland.windowManager.hyprland = {
+		enable = true;
+		settings = {
+
+
+			monitor = ",preferred,auto,auto";
+
+
+
+
+			"$terminal" = "kitty";
+			"$fileManager" = "dolphin";
+			"$apps" = "wofi --show drun";
+			"$browser" = "firefox";
+			"$menu" = "wlogout";
+			"$lock" = "swaylock";
+
+
+
+
+			exec-once = [ 
+				"$terminal"
+				"nm-applet --indicator &"
+				"waybar &"
+				"swww-daemon && swww img ~/Images/Wallpaper/1i918gwz1gl61.png"
+			];
+
+
+
+
+			env = [ 
+				"XCURSOR_SIZE,24"
+				"HYPRCURSOR_SIZE,24"
+			];
+
+
+
+
+			general = { 
+				gaps_in = 5;
+				gaps_out = 20;
+
+				border_size = 2;
+
+# https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
+				"col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+				"col.inactive_border" = "rgba(595959aa)";
+
+# Set to true enable resizing windows by clicking and dragging on borders and gaps
+				resize_on_border = false;
+
+# Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
+				allow_tearing = false;
+
+				layout = "dwindle";
+			};
+
+			decoration = {
+				rounding = 10;
+
+# Change transparency of focused and unfocused windows
+					active_opacity = 1.0;
+					inactive_opacity = 1.0;
+
+
+# https://wiki.hyprland.org/Configuring/Variables/#blur
+					blur = {
+						enabled = true;
+						size = 3;
+						passes = 1;
+						vibrancy = 0.1696;
+					};
+			};
+
+			animations = {
+				enabled = true;
+
+# Default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+
+					bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+			};
+
+			dwindle = {
+				pseudotile = true; # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section belou
+					preserve_split = true; # You probably want this
+			};
+
+			#master = {
+			#	new_status = master;
+			#};
+
+			misc = { 
+				force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
+					disable_hyprland_logo = true; # If true disables the random hyprland logo / anime girl background. :(
+							};
+
+
+
+							input = {
+							kb_layout = "fr";
+
+							follow_mouse = 1;
+
+							sensitivity = 0; # -1.0 - 1.0, 0 means no modification.
+
+							touchpad = {
+							natural_scroll = true;
+							};
+							};
+
+							gestures = {
+								workspace_swipe = false;
+							};
+
+							device = {
+								name = "epic-mouse-v1";
+								sensitivity = -0.5;
+							};
+
+
+
+							"$mainMod" = "SUPER"; # Sets "Windows" key as main modifier";
+
+							bind = [ 
+								"$mainMod, Q, exec, $terminal"
+								"$mainMod, C, killactive,"
+								"$mainMod, M, exit,"
+								"$mainMod, E, exec, $fileManager"
+								"$mainMod, V, togglefloating,"
+								"$mainMod, R, exec, $apps"
+								"$mainMod, P, pseudo, # dwindle"
+								"$mainMod, J, togglesplit, # dwindle"
+								"$mainMod, B, exec, brave"
+								"$mainMod, F, exec, firefox"
+								"$mainMod, L, exec, $lock"
+								"Control_L Alt_L, Delete, exec, $menu"
+								"$mainMod, left, movefocus, l"
+								"$mainMod, right, movefocus, r"
+								"$mainMod, up, movefocus, u"
+								"$mainMod, down, movefocus, d"
+								"$mainMod, $desk1, workspace, 1"
+								"$mainMod, $desk2, workspace, 2"
+								"$mainMod, $desk3, workspace, 3"
+								"$mainMod, $desk4, workspace, 4"
+								"$mainMod, $desk5, workspace, 5"
+								"$mainMod, $desk6, workspace, 6"
+								"$mainMod, $desk7, workspace, 7"
+								"$mainMod, $desk8, workspace, 8"
+								"$mainMod, $desk9, workspace, 9"
+								"$mainMod, $desk10, workspace, 10"
+								"$mainMod SHIFT, $desk1, movetoworkspace, 1"
+								"$mainMod SHIFT, $desk2, movetoworkspace, 2"
+								"$mainMod SHIFT, $desk3, movetoworkspace, 3"
+								"$mainMod SHIFT, $desk4, movetoworkspace, 4"
+								"$mainMod SHIFT, $desk5, movetoworkspace, 5"
+								"$mainMod SHIFT, $desk6, movetoworkspace, 6"
+								"$mainMod SHIFT, $desk7, movetoworkspace, 7"
+								"$mainMod SHIFT, $desk8, movetoworkspace, 8"
+								"$mainMod SHIFT, $desk9, movetoworkspace, 9"
+								"$mainMod SHIFT, $desk10, movetoworkspace, 10"
+								"$mainMod, S, togglespecialworkspace, magic"
+								"$mainMod SHIFT, S, movetoworkspace, special:magic"
+								"$mainMod, mouse_down, workspace, e+1"
+								"$mainMod, mouse_up, workspace, e-1"
+							];
+
+							"$desk1" = "ampersand";
+							"$desk2" = "eacute";
+							"$desk3" = "quotedbl";
+							"$desk4" = "apostrophe";
+							"$desk5" = "parenleft";
+							"$desk6" = "minus";
+							"$desk7" = "egrave";
+							"$desk8" = "underscore";
+							"$desk9" = "ccedilla";
+							"$desk10" = "agrave";
+
+							bindm = [ 
+								"$mainMod, mouse:272, movewindow"
+								"$mainMod, mouse:273, resizewindow"
+							];
+
+
+
+
+
+
+							windowrulev2 = [ 
+								"suppressevent maximize, class:.* # You'll probably like this."
+							];
+
+		};
+	};
+}
