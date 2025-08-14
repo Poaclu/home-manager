bbb72b51adf8a8b38fa29f6f07d9a53e3472602b
Poaclu <25772718+Poaclu@users.noreply.github.com>
2025-08-14 08:07:08 +0200

    Add flake & experimental features by default for non-NixOS machines

--- modules/main.nix
+++ modules/main.nix
@@ -1,4 +1,8 @@
 {
   nixpkgs.config.allowUnfree = true;
+  nix = {
+    package = pkgs.nixVersions.latest;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
   programs.home-manager.enable = true;
 }
